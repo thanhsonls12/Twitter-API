7c97f3da43e662786fb43a34afb1621e9c25998d
soncode <tuyd183@gmail.com>
2026-05-24 10:30:45 +0700

    Streaming Video

--- src/constants/httpStatus.ts
+++ src/constants/httpStatus.ts
@@ -1,6 +1,7 @@
 const httpStatus = {
   OK: 200,
   CREATED: 201,
+  PARTIAL_CONTENT: 206,
   BAD_REQUEST: 400,
   UNAUTHORIZED: 401,
   FORBIDDEN: 403,
--- src/controllers/medias.controllers.ts
+++ src/controllers/medias.controllers.ts
@@ -4,7 +4,8 @@ import mediasServices from '@/services/medias.services.js'
 
 import { Request, Response } from 'express'
 import path from 'path'
-
+import fs from 'fs'
+import mime from 'mime-types'
 export const uploadImageController = async (req: Request, res: Response) => {
   const result = await mediasServices.handleUploadImage(req)
   return res.json({
@@ -37,16 +38,27 @@ export const uploadVideoController = async (req: Request, res: Response) => {
 }
 
 export const serveVideoController = (req: Request, res: Response) => {
+  const { range } = req.headers
+  if (!range) {
+    return res
+      .status(httpStatus.BAD_REQUEST)
+      .json({ message: 'Range header is required' })
+  }
   const { filename } = req.params
-  return res.sendFile(
-    path.resolve(UPLOAD_VIDEO_DIR, filename as string),
-    (err) => {
-      if (err) {
-        console.error('Error sending file:', err)
-        return res
-          .status(httpStatus.NOT_FOUND)
-          .json({ message: 'Video not found' })
-      }
-    }
-  )
+  const videoPath = path.resolve(UPLOAD_VIDEO_DIR, filename as string)
+  const videoSize = fs.statSync(videoPath).size
+  const CHUNK_SIZE = 10 ** 6 // 1MB
+  const start = Number(range.replace(/\D/g, ''))
+  const end = Math.min(start + CHUNK_SIZE, videoSize - 1)
+  const contentLength = end - start + 1
+  const contentType = mime.lookup(videoPath) || 'video/*'
+  const headers = {
+    'Content-Range': `bytes ${start}-${end}/${videoSize}`,
+    'Accept-Ranges': 'bytes',
+    'Content-Length': contentLength,
+    'Content-Type': contentType
+  }
+  res.writeHead(httpStatus.PARTIAL_CONTENT, headers)
+  const videoStream = fs.createReadStream(videoPath, { start, end })
+  videoStream.pipe(res)
 }
